8c6da4a1047aa6cc60fd163e75db0a10d362ec84
jack2021 <jack2021@CORP.AMAZONWORKSPACES.COM>
2021-06-02 15:53:22 +0530

    update menu items with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import HomePage from './HomePage.component';
+import HomePage from './Pages/Homepage/HomePage.component';
+import {Route} from 'react-router-dom';
+
+const HatsPage =()=>
+(<div>
+  <h1>Hats Page</h1>
+</div>);
 
 function App() {
   return (
     <div className="App">
-    <HomePage />
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
---
+++ src/Pages/Homepage/HomePage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../Component/directory-items/directory-items.component';
+const HomePage = () => {
+    return (
+        <div className='HomePage'>
+            <Directory />
+        </div>
+    )
+}
+export default HomePage;
